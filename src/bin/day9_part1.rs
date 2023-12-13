3f8123e99e832eb1aee5d6ed1c41a7304a5d98e1
kuviman <kuviman@gmail.com>
2023-12-13 17:13:51 +0400

    day9

---
+++ src/bin/day9_part1.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+
+fn main() {
+    let input: Vec<Vec<i64>> = std::fs::read_to_string("input/day9")
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect()
+        })
+        .collect();
+
+    fn extrapolate(a: &[i64]) -> i64 {
+        if let Ok(value) = a.iter().copied().all_equal_value() {
+            return value;
+        }
+        let deltas: Vec<i64> = a.iter().tuple_windows().map(|(a, b)| b - a).collect();
+        let next_delta = extrapolate(&deltas);
+        *a.last().unwrap() + next_delta
+    }
+
+    let answer: i64 = input.iter().map(|a| extrapolate(a)).sum();
+    println!("{answer}");
+}
